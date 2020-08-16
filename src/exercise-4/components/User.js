55904984d6fdd973113f1fe14c6c0f0cffafad9f
ubinexy <ubinexy@gmail.com>
2020-08-17 00:01:36 +0800

    exercise 4

--- src/exercise-4/components/User.js
+++ src/exercise-4/components/User.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+import NotMatch from './NotMatch';
 
-const User = () => {
-  return (
-    <div>
-      User profile page.
-    </div>
-  );
+class User extends Component {
+  
+  render() {
+    const {match: {params} } = this.props;
+    const parsed = parseInt(params.user, 10);
+
+    if(parsed !== params.user) {
+      return <NotMatch />;
+    }
+
+    return (
+      <div>
+        User profile page.
+      </div>
+    );
+  }
+  
 };
 
 export default User;
